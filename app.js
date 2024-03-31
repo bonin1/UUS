3a830011a48f2da100de2b7f696931b11ae86515
bonin1 <74800501+bonin1@users.noreply.github.com>
2024-03-31 01:37:47 +0100

    Installing packages

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.use("/static", express.static('static'));
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Ready!')
+})
