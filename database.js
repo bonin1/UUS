f67347f24dfe9400137a1c4e5662969fca0462b3
bonin1 <74800501+bonin1@users.noreply.github.com>
2024-04-15 22:40:22 +0200

    .

--- database.js
+++ database.js
@@ -1,19 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
-
 const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
     dialect: 'mysql',
     dialectModule: require('mysql2'),
     logging: false
-
 });
 
-
 db.authenticate()
-    .then(() => console.log("connection has been established"))
+    .then(() => {
+        console.log("connection has been established");
+        db.query(`CREATE DATABASE IF NOT EXISTS uusdb;`)
+            .then(() => console.log('Database created or successfully checked'))
+            .catch(err => console.error('Error in creating database', err));
+    })
     .catch(err => console.error("unable to connect to the database:", err));
 
-
-    module.exports = db
+module.exports = db;
