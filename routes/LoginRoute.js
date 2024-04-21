2941abb949c55774936504974eca3029c3dc3f36
bonin1 <74800501+bonin1@users.noreply.github.com>
2024-04-21 22:28:50 +0200

    login(important)

--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -35,4 +35,46 @@ router.get('/', (req, res) => {
     res.render('login', { message: '', csrfToken});
 });
 
+
+router.post('/', async (req, res) => {
+    if(req.body._csrf !== req.session.csrfToken) {
+        return res.status(401).send('Invalid CSRF token');
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!validator.isEmail(email)) {
+        return res.render('login', { message: 'Invalid email address',csrfToken: req.session.csrfToken });
+    }
+
+    if (!validator.isLength(password, { min: 8 })) {
+        return res.render('login', { message: 'password must be 8 characters',csrfToken: req.session.csrfToken});
+    }
+
+    try {
+        const user = await LoginInformation.findOne({ where: { email: email } });
+        if (!user) {
+            return res.render('login', { message: 'incorrect email or password',csrfToken: req.session.csrfToken });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.render('login', { message: 'incorrect email or password',csrfToken: req.session.csrfToken });
+        }
+
+        const rememberMe = req.body.rememberMe === 'on';
+        
+        if (rememberMe) {
+            const rememberToken = crypto.randomBytes(64).toString('hex');
+            res.cookie('rememberToken', rememberToken, { maxAge: 30 * 24 * 60 * 60 * 1000 });
+        }
+        
+        req.session.isLoggedIn = true;
+        req.session.userId = user.id;
+        res.redirect('/e-learning');
+    } catch (err) {
+        console.log(err);
+        return res.render('login', { message: 'An error occurred while processing your request', csrfToken: req.session.csrfToken });
+    }
+});
+
 module.exports = router;
