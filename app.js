4c3d7db4f2c73fed45a72d6fd7ab87e9eecdf571
bonin1 <74800501+bonin1@users.noreply.github.com>
2024-04-22 02:07:51 +0200

    ..

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const express = require("express")
 const app = express()
 const bodyParser = require('body-parser');
 require('dotenv').config();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.use("/static", express.static('static'));
+const session = require('express-session');
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        sameSite: 'strict'
+    }
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+    res.locals.isLoggedIn = req.session.isLoggedIn;
+    next();
+});
+
+app.get('/e-learning',(req,res)=>{
+    if (!req.session.isLogged) {
+        return res.redirect('/login');
+    }
+    res.render('e-learning')
+})
 
 const User = require('./model/UsersModel')
 const Feedback = require('./model/FeedbackModel')
@@ -54,6 +77,18 @@ routes.forEach(route => {
 app.get('*', (req, res) => {
     res.render('error');
 });
+app.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.error(err);
+        }
+        res.clearCookie('rememberToken');
+        res.redirect('/login');
+    });
+});
+
+
+
 
 app.listen(process.env.PORT, ()=>{
     console.log('Ready!')
--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -29,7 +29,21 @@ router.use(session({
 router.use(bodyParser.json({ limit: '50mb' }));
 router.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+router.use((req, res, next) => {
+    if (req.cookies.rememberToken && !req.session.isLoggedIn) {
+        req.session.isLoggedIn = true;
+        req.session.userId = req.cookies.rememberToken;
+        return res.redirect('/e-learning');
+    } else {
+        next();
+    }
+});
+
+
 router.get('/', (req, res) => {
+    if (req.cookies.rememberToken) {
+        return res.redirect('/e-learning');
+    }
     const csrfToken = crypto.randomBytes(64).toString('hex');
     req.session.csrfToken = csrfToken;
     res.render('login', { message: '', csrfToken});
@@ -50,7 +64,7 @@ router.post('/', async (req, res) => {
     if (!validator.isLength(password, { min: 8 })) {
         return res.render('login', { message: 'password must be 8 characters',csrfToken: req.session.csrfToken});
     }
-
+    
     try {
         const user = await LoginInformation.findOne({ where: { email: email } });
         if (!user) {
