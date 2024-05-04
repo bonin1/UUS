90eef8e59441562246b8061838ddc98ec0c9ee78
bonin1 <74800501+bonin1@users.noreply.github.com>
2024-05-05 01:55:29 +0200

    ..

--- app.js
+++ app.js
@@ -666,36 +666,24 @@ app.post('/change-password', async (req, res) => {
 const sessions = {};
 
 app.post('/message', (req, res) => {
-    const sessionId = req.body.sessionId;
     const message = req.body.message;
-
-    let session = sessions[sessionId];
-    if (!session) {
-        session = { stage: 'start' }; 
-        sessions[sessionId] = session;
-    }
 
     let responseMessage;
 
-    switch (session.stage) {
-        case 'start':
-            responseMessage = 'Welcome! What would you like to do?';
-            session.stage = 'chooseOption';
-            break;
-        case 'chooseOption':
-            if (message === 'Option 1') {
+    if (!message) {
+        responseMessage = 'Welcome! What would you like to do?';
+    } else {
+        switch (message.toLowerCase()) {
+            case 'option 1':
                 responseMessage = 'You chose Option 1. Redirecting to Option 1 page...';
-                session.stage = 'start';
-            } else if (message === 'Option 2') {
+                break;
+            case 'option 2':
                 responseMessage = 'You chose Option 2. Redirecting to Option 2 page...';
-                session.stage = 'start';
-            } else {
+                break;
+            default:
                 responseMessage = 'Please choose a valid option (Option 1 or Option 2).';
-            }
-            break;
-        default:
-            responseMessage = 'Sorry, I didn\'t understand that.';
-            break;
+                break;
+        }
     }
 
     res.json({ message: responseMessage });
